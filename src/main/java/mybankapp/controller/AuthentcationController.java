2ac6918343f9655edd17a83eb8aa2c38f0470e52
Yuriy Gorelov <gur-ok@yandex.ru>
2021-03-26 17:24:47 +0400

    add AuthService

--- src/main/java/mybankapp/controller/AuthentcationController.java
+++ src/main/java/mybankapp/controller/AuthentcationController.java
@@ -1,24 +1,14 @@
 package mybankapp.controller;
 
-import io.jsonwebtoken.impl.DefaultClaims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import mybankapp.dto.AuthenticationRequestDTO;
-import mybankapp.model.Person;
-import mybankapp.security.JwtUserDetailsService;
-import mybankapp.security.jwt.JwtTokenProvider;
-import mybankapp.service.PersonService;
+import mybankapp.security.AuthService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -26,56 +16,15 @@ import java.util.Map;
 @RequestMapping(value = "/bank")
 public class AuthentcationController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final PersonService personService;
-    private final JwtUserDetailsService jwtUserDetailsService;
+    private final AuthService authService;
 
     @PostMapping(value = "/login")
     public ResponseEntity login(@RequestBody AuthenticationRequestDTO dto){
-        try {
-            String username = dto.getUsername();
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, dto.getPassword()));
-            Person person = personService.getPersonByName(username);
-
-            if (person == null) {
-                throw new UsernameNotFoundException("user not found");
-            }
-
-            String accessToken = jwtTokenProvider.createToken(username, person.getRoles());
-            String freshToken = jwtTokenProvider.createRefreshToken(username, person.getRoles());
-            Map<Object, Object> response = new HashMap<>();
-            response.put("Username", username);
-            response.put("AccessToken", accessToken);
-            response.put("RefreshToken", freshToken);
-
-            return ResponseEntity.ok(response);
-        } catch (AuthenticationException e) {
-            throw new BadCredentialsException("Invalid username or password");
-        }
+       return authService.login(dto);
     }
 
     @GetMapping(value = "/refreshtoken/")
     public ResponseEntity refreshToken(HttpServletRequest request){
-
-            System.out.println("IN REFRESH CONTROLLER!!");
-            //Get claims from request
-            DefaultClaims claims = (DefaultClaims) request.getAttribute("claims");
-            String username = claims.get("sub", String.class);
-            log.info("In refreshController. " + username);
-
-            Person person = personService.getPersonByName(username);
-
-            if (person == null) {
-                throw new UsernameNotFoundException("user not found");
-            }
-            //Give new Access token!
-            String token = jwtTokenProvider.createToken(username, person.getRoles());
-
-            Map<Object, Object> response = new HashMap<>();
-            response.put("username", username);
-            response.put("New Access token", token);
-
-            return ResponseEntity.ok(response);
+        return authService.refresh(request);
     }
 }
